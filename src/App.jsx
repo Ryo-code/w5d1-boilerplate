fe0818818b78d5497a8f6525b1385b078a992daa
Ryo <kanadajinlee@yahoo.ca>
2016-11-29 21:05:01 +0000

    I got the template of the Chatty App. ...and I turned it purple

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+
+/* * * * * * * ES Version Comparison * * * * * * * * * * * *
+//ES5: "var App = React.createClass({render: function(){console.log("hi")} });"...
+//ES6: "class App extends React.Component { render(){console.log("hi")} }"...
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 class App extends Component {
   render() {
+    // console.log("Rendering <App/>");
     return (
-      <h1>Hello React :)</h1>
+      <div className="wrapper">
+        <nav>
+          <h1>Chatty</h1>
+        </nav>
+        <MessageList/>
+
+        <ChatBar/>
+      </div>
     );
   }
 }
---
+++ src/MessageList.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    // console.log("Rendering <MessageList/>");
+    return (
+      <div id="message-list">
+        <Message/>
+        <div class="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MessageList;
